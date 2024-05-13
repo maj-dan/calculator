a468dbdcd6c1948fd7daddf25c61467ff4b82872
maj-dan <160074910+maj-dan@users.noreply.github.com>
2024-05-12 23:01:53 -0300

    Select one math function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
+let firstNumber,
+    secondNumber,
+    operator;
 
 
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "add":
+            return add(num1, num2);
+        case "sub":
+            return subtract(num1, num2);
+        case "mul":
+            return multiply(num1, num2);
+        case "div":
+            return divide(num1, num2);
+    }
+}
 
 function add(num1, num2) {
     return num1 + num2;
